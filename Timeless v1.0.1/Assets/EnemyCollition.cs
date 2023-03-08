c4f39d7dc01cf7b112a20be15cc72dc8e017dc67
Ramirez-Facu <rfacundo1995@gmail.com>
2023-03-08 02:08:43 -0300

    Add a particle effect to the movement to the player
    also delete the Power up transformation (it was for testing)

--- Timeless v1.0.1/Assets/EnemyCollition.cs
+++ Timeless v1.0.1/Assets/EnemyCollition.cs
@@ -13,10 +13,10 @@ public class EnemyCollition : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        void OnCollisionEnter2D(Collision2D collision)
-        {
-         Application.Quit();
-        }
+    //    void OnCollisionEnter2D(Collision2D collision)
+    //    {
+    //     Application.Quit();
+    //    }
     }
 }
 
--- Timeless v1.0.1/Assets/PlayerController.cs
+++ Timeless v1.0.1/Assets/PlayerController.cs
@@ -16,7 +16,7 @@ public class PlayerController : MonoBehaviour
     private float dashTimer;
     private float dashCooldownTimer;
     private Vector3 normalColliderSize;
-
+    private bool Immunity = false;
 
     public float jumpForce = 2f;
     public float crouchScale = 0.5f;
@@ -30,8 +30,10 @@ public class PlayerController : MonoBehaviour
 
     //*********************************************************************
 
+    //********************* Effects ***************************************
+    public ParticleSystem Dust;
     //************************ Boolean Variables **************************
-    private bool powerupActive = false;
+    //private bool powerupActive = false;
     private bool isDashing;
     private bool isJumping = false;
     private bool isCrouching = false;
@@ -49,11 +51,6 @@ public class PlayerController : MonoBehaviour
     private SpriteRenderer spriteRender;
     //*********************************************************************
 
-
-    private float crouchTimer = 0f;
-
-
-
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -64,205 +61,107 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.R))
+        // Movement to the sides
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        if (horizontalInput != 0)
         {
-            powerupActive = !powerupActive;
-            print(powerupActive);
-            if (powerupActive)
+            CreateDust();
+            anim.SetBool("running", true);
+            transform.position += new Vector3(horizontalInput * moveSpeed * Time.deltaTime, 0f, 0f);
+            if (horizontalInput > 0)
             {
-                anim.SetBool("PowerUp", true);
+                spriteRender.flipX = false;
+                spriteRender.sprite = RunnigSprite;
             }
             else
             {
-                anim.SetBool("PowerUp", false);
+                spriteRender.flipX = true;
+                spriteRender.sprite = RunnigSprite;
             }
+
         }
-        if (Input.GetKeyDown(KeyCode.E))
+        else
         {
-            powerupActive = !powerupActive;
-            print(powerupActive);
-            if (powerupActive)
+            anim.SetBool("running", false);
+            if (!isJumping)
             {
-                anim.SetBool("PowerUp", true);
+                StopDust();
+                spriteRender.sprite = idleSprite;
             }
-            else
+            else 
             {
-                anim.SetBool("PowerUp", false);
-            }
-
-            }
-        if (!powerupActive)
-        {
-            // Movement to the sides
-            float horizontalInput = Input.GetAxisRaw("Horizontal");
-            if (horizontalInput != 0)
-            {
-
-                anim.SetBool("running", true);
-                transform.position += new Vector3(horizontalInput * moveSpeed * Time.deltaTime, 0f, 0f);
-                if (horizontalInput > 0)
-                {
-                    spriteRender.flipX = false;
-                    spriteRender.sprite = RunnigSprite;
-                }
-                else
-                {
-                    spriteRender.flipX = true;
-                    spriteRender.sprite = RunnigSprite;
-                }
-
-            }
-            else
-            {
-                anim.SetBool("running", false);
-                if (!isJumping)
-                {
-                    spriteRender.sprite = idleSprite;
-                }
-                else 
-                {
-                    spriteRender.sprite = jumpingSprite;
-                }
-                
-            }
-
-            ////dashing
-
-            if (Input.GetKeyDown(KeyCode.C) && dashCooldownTimer <= 0)
-            {
-                Dash();
-            }
-
-            // Check if dash is still in progress
-            if (isDashing)
-            {
-                // Subtract from dash timer
-                dashTimer -= Time.deltaTime;
-
-                // Check if dash is over
-                if (dashTimer <= 0)
-                {
-                    // End dash
-                    isDashing = false;
-                    rb.velocity = Vector2.zero;
-                    dashCooldownTimer = dashCooldown;
-                }
-            }
-            else
-            {
-                // Start cooldown timer
-                if (dashCooldownTimer > 0)
-                {
-                    dashCooldownTimer -= Time.deltaTime;
-                }
-            }
-
-            // Jumping
-            if (Input.GetKeyDown(KeyCode.Space) && !isJumping && !isCrouching)
-            {
-                isJumping = true;
-                rb.velocity = new Vector2(rb.velocity.x, jumpForce);
-                spriteRender = GetComponent<SpriteRenderer>();
                 spriteRender.sprite = jumpingSprite;
-
             }
-
-            // Crouching
-            if (Input.GetKeyDown(KeyCode.X) && !isJumping && !isCrouching)
-            {
-                isCrouching = true;
-                GetComponent<BoxCollider2D>().size = new Vector2(GetComponent<BoxCollider2D>().size.x, normalColliderSize.y * crouchScale);
-                StartCoroutine(CrouchTimer());
-            }
-
-            if (isCrouching)
-            {
-                GetComponent<Rigidbody2D>().velocity = Vector2.zero;
-            }
-
-            if (isJumping || isCrouching)
-            {
-                GetComponent<BoxCollider2D>().enabled = true;
-            }
-            else
-            {
-                GetComponent<BoxCollider2D>().enabled = true;
-            }
-        }
-        else 
-        {
-
-            // Movement to the sides
-            float horizontalInput = Input.GetAxisRaw("Horizontal");
-            if (horizontalInput != 0)
-            {
-                anim.SetBool("running", true);
-                transform.position += new Vector3(horizontalInput * moveSpeed * Time.deltaTime, 0f, 0f);
-                if (horizontalInput > 0)
-                {
-                    spriteRender.flipX = false;
-                    spriteRender.sprite = PowerupRunnigSprite;
-                }
-                else
-                {
-                    spriteRender.flipX = true;
-                    spriteRender.sprite = PowerupRunnigSprite;
-                }
-
-            }
-            else
-            {
-                anim.SetBool("running", false);
-                if (!isJumping)
-                {
-                    spriteRender.sprite = PowerupIdleSprite;
-                }
-                else
-                {
-                    spriteRender.sprite = PowerupjumpingSprite;
-                }
                 
-            }
+        }
 
-            // Jumping
-            if (Input.GetKeyDown(KeyCode.Space) && !isJumping && !isCrouching)
-            {
-                isJumping = true;
-                rb.velocity = new Vector2(rb.velocity.x, jumpForce*1.4f);
-                spriteRender = GetComponent<SpriteRenderer>();
-                spriteRender.sprite = PowerupjumpingSprite;
+        ////dashing
 
-            }
+        if (Input.GetKeyDown(KeyCode.C) && dashCooldownTimer <= 0)
+        {
+            CreateDust();
+            Dash();
+                
+        }
 
-            // Crouching
-            if (Input.GetKeyDown(KeyCode.LeftControl) && !isJumping && !isCrouching)
-            {
-                isCrouching = true;
-                GetComponent<BoxCollider2D>().size = new Vector2(GetComponent<BoxCollider2D>().size.x, normalColliderSize.y * crouchScale);
-                StartCoroutine(CrouchTimer());
-            }
+        // Check if dash is still in progress
+        if (isDashing)
+        {
+                
+            Immunity = true;
+            // Subtract from dash timer
+            dashTimer -= Time.deltaTime;
 
-            if (isCrouching)
+            // Check if dash is over
+            if (dashTimer <= 0)
             {
-                GetComponent<Rigidbody2D>().velocity = Vector2.zero;
-            }
-
-            if (isJumping || isCrouching)
-            {
-                GetComponent<BoxCollider2D>().enabled = true;
-            }
-            else
-            {
-                GetComponent<BoxCollider2D>().enabled = true;
+                // End dash
+                isDashing = false;
+                rb.velocity = Vector2.zero;
+                dashCooldownTimer = dashCooldown;
             }
         }
-    }
+        else
+        {
+            // Start cooldown timer
+            if (dashCooldownTimer > 0)
+            {
+                Immunity = false;
+                dashCooldownTimer -= Time.deltaTime;
+            }
+        }
 
-    IEnumerator CrouchTimer()
-    {
-        yield return new WaitForSeconds(crouchDuration);
-        isCrouching = false;
-        GetComponent<BoxCollider2D>().size = normalColliderSize;
+        // Jumping
+        if (Input.GetKeyDown(KeyCode.Space) && !isJumping && !isCrouching)
+        {
+            isJumping = true;
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            spriteRender = GetComponent<SpriteRenderer>();
+            spriteRender.sprite = jumpingSprite;
+
+        }
+
+        // Crouching
+        if (Input.GetKeyDown(KeyCode.X) && !isJumping && !isCrouching)
+        {
+            isCrouching = true;
+            GetComponent<BoxCollider2D>().size = new Vector2(GetComponent<BoxCollider2D>().size.x, normalColliderSize.y * crouchScale);
+        }
+
+        if (isCrouching)
+        {
+            GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+        }
+
+        if (isJumping || isCrouching)
+        {
+            GetComponent<BoxCollider2D>().enabled = true;
+        }
+        else
+        {
+            GetComponent<BoxCollider2D>().enabled = true;
+        }
+        
     }
 
     void OnCollisionEnter2D(Collision2D collision)
@@ -274,11 +173,18 @@ public class PlayerController : MonoBehaviour
         }
     }
 
+    void CreateDust() 
+    {
+        Dust.Play();
+    }
+    void StopDust()
+    {
+        Dust.Stop();
+    }
     void Dash()
     {
         // Get movement direction
         movement = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
-
         // Set dash timer and flag
         dashTimer = dashDuration;
         isDashing = true;
@@ -286,4 +192,5 @@ public class PlayerController : MonoBehaviour
         // Set velocity to dash distance in movement direction
         rb.velocity = movement * dashDistance;
     }
+
 }
